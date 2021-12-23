2d67f7b12c1ae968f9f9491c575941e581973ea6
H7-Dev <herbert.dev.alfa@gmail.com>
2021-12-23 11:44:46 -0300

    00.01 save-g

---
+++ www/ASSETS/JS/els.js
@@ -0,0 +1,14 @@
+console.log('Dispositivo | Web | build: 14/15');
+const els = {
+        doc: $(document),
+        ripple: $('.ripple'),
+        btn_entrarApp: $('#btn_entrarApp'),
+        devlopPage: $('#devlopPage'),
+        conectando: $('#conectando'),
+        conectado: $('#conectado'),
+        btn_testeDynElEvents: $('#btn_testeDynElEvents'),
+        btn_testAppVar: $('#btn_testAppVar'),
+    init: () => {
+    }
+}
+els.init()
