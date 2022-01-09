6cc8289966f94e84642d7a1d8611b3c5c3ba6546
H7-Dev <herbert.dev.alfa@gmail.com>
2022-01-09 10:19:40 -0300

    02.02 save(leiaute selFoto 1.0.0)

--- www/ASSETS/COMPONENTS/JS/selFoto.1.0.0.js
+++ www/ASSETS/COMPONENTS/JS/selFoto.1.0.0.js
@@ -1,6 +1,7 @@
 const selcFoto = {
     doc: $(document),
     imgSrc: '.selFoto > .imgSrcOptions',
+    btn_fehcar: '.overOpcaoSrcImg > .ctOpcao > header > .btnFechar',
     init: () => {
         setTimeout(function () {
             console.log('init selcFoto')
@@ -8,14 +9,25 @@ const selcFoto = {
         }, 400);
     },
     Ouvintes: () => {
-        selcFoto.doc.on('click', selcFoto.imgSrc,function(e){
-            console.log('evento onClick (T-00.00)')
+        // * fechar/remover/ocultar .overOpcaoSrcImg
+        selcFoto.doc.on('click', selcFoto.btn_fehcar,function(e){
+            console.log('Teste')
             let $this = $(this)
-            console.log($this)
             setTimeout(function() {
-                alert("Teste ok")
+                selcFoto.rm_overOpcaoSrcImg($this)
             }, 1000);
         })
-    }
+        // selcFoto.doc.on('click', selcFoto.imgSrc,function(e){
+        //     console.log('evento onClick (T-00.00)')
+        //     let $this = $(this)
+        //     console.log($this)
+        //     setTimeout(function() {
+        //         alert("Teste ok")
+        //     }, 1000);
+        // })
+    },
+    rm_overOpcaoSrcImg: function(_el){
+        _el.closest('.overOpcaoSrcImg').remove()
+    },
 }
 selcFoto.init()
